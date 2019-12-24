645ee147a415b1df58dabe7f2776260ee822bb99
Fitti <28876473+fittiboy@users.noreply.github.com>
2019-12-24 03:59:25 +0100

    Fix setup.sh errors

--- setup.sh
+++ setup.sh
@@ -5,7 +5,8 @@ apt install git python3.8 tmux vim zsh -y
 export ZSH=$HOME/.oh-my-zsh
 git clone https://github.com/fittiboy/dotfiles ~/dotfiles
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-python3.8 ~/dotfiles/export.py
+rm -f .zshrc .vimrc .tmux.conf
+python3.8 $HOME/dotfiles/export.py
+chsh -s /bin/zsh
 
