153973c5b4a74ff7630d919587414a9d455de5cb
Fitti <28876473+fittiboy@users.noreply.github.com>
2019-12-24 03:31:36 +0100

    Change order of commands in setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ apt install git python3.8 tmux vim zsh -y
 export ZSH=$HOME/.oh-my-zsh
 git clone https://github.com/dotfiles ~/dotfiles
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 python3.8 ~/dotfiles/export.py
-chsh -s /bin/zsh
 
