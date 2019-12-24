c7ed03633445813bd20f5e71fbbec0882f3b56e3
Fitti <28876473+fittiboy@users.noreply.github.com>
2019-12-24 03:17:56 +0100

    Readd removed line

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ apt update
 apt full-upgrade -y
 apt install python3.8 tmux vim zsh -y
 export ZSH=$HOME/.oh-my-zsh
+git clone https://github.com/dotfiles ~/dotfiles
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 python3.8 ~/dotfiles/export.py
