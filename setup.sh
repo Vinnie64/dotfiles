a06c5bd1d0a861f11bf04c95d85688e41e41be22
Fitti <28876473+fittiboy@users.noreply.github.com>
2019-12-24 04:20:46 +0100

    Remove need for sudo when calling setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-apt update
-apt full-upgrade -y
-apt install git python3.8 tmux vim zsh -y
+sudo apt update
+sudo apt full-upgrade -y
+sudo apt install git python3.8 tmux vim zsh -y
 export ZSH=$HOME/.oh-my-zsh
 git clone https://github.com/fittiboy/dotfiles ~/dotfiles
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-rm -f .zshrc .vimrc .tmux.conf
+sudo rm -f .zshrc .vimrc .tmux.conf
 python3.8 $HOME/dotfiles/export.py
 chsh -s /bin/zsh
 
