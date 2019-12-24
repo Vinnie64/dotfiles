7263d1a8e7eb8efce73abbda69b4a164435891c8
Fitti <28876473+fittiboy@users.noreply.github.com>
2019-12-24 03:40:48 +0100

    Fix dotfiles url typo

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ apt update
 apt full-upgrade -y
 apt install git python3.8 tmux vim zsh -y
 export ZSH=$HOME/.oh-my-zsh
-git clone https://github.com/dotfiles ~/dotfiles
+git clone https://github.com/fittiboy/dotfiles ~/dotfiles
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
