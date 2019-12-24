8961024ba0a3a1d2a7d9dca8ad772173bc621283
Fitti <28876473+fittiboy@users.noreply.github.com>
2019-12-24 03:14:51 +0100

    Remove unnecessary line from setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@ apt update
 apt full-upgrade -y
 apt install python3.8 tmux vim zsh -y
 export ZSH=$HOME/.oh-my-zsh
-git clone https://github.com/Fittiboy/dotfiles ~/dotfiles
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 python3.8 ~/dotfiles/export.py
