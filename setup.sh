def086e24d610c7d604fe34ab1b9f5a651527098
Fitti <dev@fitti.io>
2020-01-04 22:40:44 +0100

    Start migrating auto-install to arch

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
-sudo apt update
-sudo apt full-upgrade -y
-sudo apt install git python3.8 tmux vim zsh neomutt offlineimap python3-pip -y
+#!/bin/sh
+# Update mirrorlist
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Fittiboy/personal-scripts/master/mirrorlist-update)"
+# pacman -S " 
 export ZSH=$HOME/.oh-my-zsh
 git clone https://github.com/fittiboy/dotfiles ~/dotfiles
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 sudo rm -f .zshrc .vimrc .tmux.conf
 mkdir -p $HOME/.config/i3/
+mkdir -p $HOME/.config/alacritty/
 python3.8 $HOME/dotfiles/export.py
 chsh -s /bin/zsh
 vim +PluginInstall +qall
