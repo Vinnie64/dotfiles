a8275c7d4edb563944a6b90db5ebaa98d823b333
Fitti <dev@fitti.io>
2020-01-20 21:00:04 +0100

    Remove remaining hardcoded usernames

--- export.py
+++ export.py
@@ -12,6 +12,7 @@ with open(f'/home/{username}/dotfiles/files_to_copy.json') as filesfile:
     files = json.load(filesfile)
 
 for file in files:
+    file = file.format(username=username)
     namelist = file.split("/")
     filename = namelist[-1]
     shutil.copy(f"/home/{username}/dotfiles/{filename}", f"{file}")
