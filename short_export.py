91884c4371252b47f08d645274d661a193596584
Fitti <dev@fitti.io>
2020-02-06 23:47:19 +0100

    Create short_export.py

---
+++ short_export.py
@@ -0,0 +1,18 @@
+#!/bin/python
+from pathlib import Path
+from sys import argv
+import shutil
+import json
+import pwd
+import os
+
+username = argv[1]
+
+with open(f'/home/{username}/dotfiles/files_to_copy_short.json') as filesfile:
+    files = json.load(filesfile)
+
+for file in files:
+    file = file.format(username=username)
+    namelist = file.split("/")
+    filename = namelist[-1]
+    shutil.copy(f"/home/{username}/dotfiles/{filename}", f"{file}")
